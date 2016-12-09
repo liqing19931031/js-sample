d362a6d71223b71458eb93f368127fd18fc09c1b
liqing19931031 <m15757104968_1@163.com>
2016-12-09 10:03:00 +0800

    js特效

--- 关于js事件/index.js
+++ 关于js事件/index.js
@@ -1,10 +1,8 @@
+"use strict";
 let a = document.getElementsByClassName('dom1');
 let b = document.getElementsByClassName('dom2');
 let c = document.getElementsByClassName('dom3');
 let d = document.getElementsByTagName('a');
-a[0].addEventListener('click', function(){
-	console.log('a');
-});
 b[0].addEventListener('click', function(e){
 	console.log(e);
 	e.preventDefault();
@@ -16,4 +14,33 @@ c[0].addEventListener('click', function(e){
 d[0].onClick = function(e){
 	e.preventDefault();
 	alert(1);
+}
+var f = 2;
+function foo(str){
+	eval(str)
+	var e = 2;
+	console.log(f + e);
+}
+
+foo('var f = 1');
+(function(){
+	function abc(a){
+		a.forEach(function(){
+			console.log(this);
+			(function(){
+				console.log(this)
+			})();
+		})
+		console.log(this, a);
+	}
+	abc.call(['abc', 'bcd'], ['aaa', 'bbb']);
+})();
+
+function proccess(data){
+	console.log(data.name);
+}
+var x = true;
+if(x) {
+	let abc = { name: 'liqing'};
+	proccess ( abc )
 }
